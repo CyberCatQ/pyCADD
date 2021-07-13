9d2d2f7d7a9148fa4b6ffa558fe361afe670e884
CoffeeCat-Q <544158656@qq.com>
2021-07-13 21:38:01 +0800

    test bug fix

--- src/query.py
+++ src/query.py
@@ -13,41 +13,40 @@ root_path = os.path.abspath(os.path.dirname(__file__)).split('src')[0]  # 根路
 lib_path = root_path + 'lib/'                                           # 库文件夹路径
 doc_path = root_path + 'doc/'                                           # 文档文件夹路径
 
-try:
-    list_dir_path = lib_path + 'list/'                                  # 记录基因PDBID的列表文件库
-    os.makedirs(list_dir_path)
-except FileExistsError:
-    pass
+pdblist_dir_path = lib_path + 'pdbid/'                              # 记录基因PDBID的列表文件库
+inputfile_dir_path = lib_path + 'list/'                             # py4schrodinger输入文件库
+json_dir_path = lib_path + 'json/'                                  # 服务器返回的基因原始信息json库
+info_dir_path = lib_path + 'info/'                                  # 基因晶体信息转换后的csv格式文件库
+bdaffinity_dir_path = lib_path + 'BDaffinity/'                      # 亲和力实验数据库
 
-try:
-    json_dir_path = lib_path + 'json/'                                  # 服务器返回的基因原始信息json库
-    os.makedirs(json_dir_path)
-except FileExistsError:
-    pass
+def make_dir(path):
+    try:
+        os.makedirs(path)
+    except FileExistsError:
+        pass
 
-try:
-    info_dir_path = lib_path + 'info/'                                  # 基因晶体信息转换后的csv格式文件库
-    os.makedirs(info_dir_path)
-except FileExistsError:
-    pass
-
-try:
-    bdaffinity_dir_path = lib_path + 'BDaffinity/'                      # 亲和力实验数据库
-    os.makedirs(bdaffinity_dir_path)
-except FileExistsError:
-    pass
+make_dir(pdblist_dir_path)
+make_dir(inputfile_dir_path)
+make_dir(json_dir_path)
+make_dir(info_dir_path)
+make_dir(bdaffinity_dir_path)
 
 class Query:
     '''
 与RCSB PDB服务器交互 获取基因相关晶体信息
     '''
 
-    def __precess_inputfile(self):
+    def __init__(self) -> None:
+
+        self.gene_file = ''         # 全部基因列表文件
+        self.gene_list = []         # 全部基因列表
+        self.gene_dic = {}          # 全部基因名称对照字典
+
+    def _precess_inputfile(self):
         '''
         解析输入文件
         '''
-
-        gene_file = sys.arg[1]      # 从命令行解析提供的基因列表文件
+        gene_file = sys.argv[1]      # 从命令行解析提供的基因列表文件
 
         try:
             dic = {}
@@ -66,14 +65,6 @@ class Query:
             print('Error: File %s not Found' % gene_file)
             raise
 
-    def __init__(self) -> None:
-
-        self.__precess_inputfile()
-        self.gene_file = ''         # 全部基因列表文件
-        self.gene_list = []         # 全部基因列表
-        self.gene_dic = {}          # 全部基因名称对照字典
-
-
     def search(self, gene_list=[]):
         '''
         在RCSB服务器搜索列表中的基因相关晶体PDBID 存储为文本文件
@@ -83,17 +74,27 @@ class Query:
         gene_list : list
             要搜索的全部基因列表
         '''
-        os.chdir(list_dir_path)
+        os.chdir(pdblist_dir_path)
         if not gene_list:
             gene_list = self.gene_list
 
+        print('\nAll Genes:', gene_list)
+        print('Searching on RCSB Server...')
+
         for gene in gene_list:
             gene = gene.strip()
+            print('\n[Searching %s]' % gene)
             query = attrs.rcsb_entity_source_organism.rcsb_gene_name.value == gene
             results = set(query())
+            if results:
+                print('%s Search Results:' % gene, results)
+            else:
+                print('%s Search Results:' % gene, None)
             with open(gene + '.txt','w') as file:
                 for id in results:
                     file.write(id + '\n')
+        
+        print('Search Complete.')
 
     def __save_data(self, request, gene):
         '''
@@ -136,7 +137,7 @@ class Query:
         gene_list: list
             要请求的基因列表
         '''
-
+        print('Query Crystals Info from RCSB Server...')
         if not gene_list:
             gene_list = self.gene_list                                          # 读取所有列表文件名称
 
@@ -145,10 +146,14 @@ class Query:
         for gene in gene_list:
             pdbids = ''
             gene = gene.strip()
+            
+            if os.path.exists(json_dir_path + gene + '.json'):
+                print('%s Crystals Information Already Exists. Skip.' % gene)
+                continue
+            print('[Get %s Crystals Information]' % gene)
 
-            with open(list_dir_path + gene + '.txt', 'r') as f:
+            with open(pdblist_dir_path + gene + '.txt', 'r') as f:
                 pdbs = f.readlines()
-
                 if pdbs:                                                        # 将所有PDBID 以逗号分割并相连 组成请求字符串
                     for i in pdbs:
                         pdbids = pdbids + "\"" + i.strip() + "\"" + ','
@@ -215,11 +220,14 @@ class Query:
 	}
 }
     ''' % pdbids
-                query = parse.quote(query)              # 将请求内容以url编码
-                request = url + query                   # 组装请求url
+                    query = parse.quote(query)              # 将请求内容以url编码
+                    request = url + query                   # 组装请求url
 
-                self.__save_data(request, gene)         # 发送请求 保存返回数据为json文件
-                sleep(2)
+                    self.__save_data(request, gene)         # 发送请求 保存返回数据为json文件
+                    sleep(2)
+                else:
+                    print('WARNING: %s.txt has no crystal.' % gene)
+        print('\nQuery Complete.')
 
 
     def convert_csv(self, gene_list=[]):
@@ -260,10 +268,11 @@ class Query:
             except FileNotFoundError:
                 pass
             '''
-
-            jsfile = open(json_dir_path + gene, 'r')
+            try:
+                jsfile = open(json_dir_path + gene + '.json', 'r')
+            except FileNotFoundError:
+                continue
             js_data = json.load(jsfile)
-
             lis_data = js_data['data']['entries']
             polymer_num = 0
             nonpolymer_num = 0
@@ -357,7 +366,7 @@ class Query:
                 del d['nonpolymer_entities']
 
                 if d['binding_affinity']:
-                    with open('BDaffinity/' + d['PDBID'] + '_binding_affinity.csv', 'w') as f:
+                    with open(lib_path + 'BDaffinity/' + d['PDBID'] + '_binding_affinity.csv', 'w') as f:
                         df = pd.DataFrame(d['binding_affinity'], columns=[
                                         'comp_id', 'value', 'provenance_code', 'type', 'unit'])
                         df.sort_values(
@@ -438,8 +447,11 @@ class Query:
         for gene in gene_list:
             
             csv_file = info_dir_path + gene + '.csv'
-            f = open(csv_file, 'r')
-            inputfile = open(list_dir_path + gene + '.txt', 'w')
+            try:
+                f = open(csv_file, 'r')
+            except FileNotFoundError:
+                continue
+            inputfile = open(inputfile_dir_path + gene + '.txt', 'w')
             reader = csv.reader(f)
 
             line1 = next(reader)
@@ -482,6 +494,7 @@ def ui():
     print('Gene Query Module'.center(80))
     print(''.center(80, '*'))
     query = Query()
+    query._precess_inputfile()
 
     while True:
         flag = input('''
@@ -492,7 +505,8 @@ Input Query Code:
 3. Make Input Files for py4schrodinger.py
 
 0. Exit
-    ''')
+
+Code:''')
 
         if flag == '1':
             query.search()
@@ -505,9 +519,6 @@ Input Query Code:
         elif flag == '0':
             break
 
-def main():
-    ui()
-
 def _usage():
     '''
     pass
@@ -517,8 +528,7 @@ if __name__ == '__main__':
     if len(sys.argv) == 1:
         print(_usage.__doc__)
     else:
-        main()
-        
+        ui()
         
         
     
