04fc7ce86e0de243ed0985b89b10a266115ecfb2
CyberCatQ <yuhangxmu@stu.xmu.edu.cn>
2022-05-19 20:25:30 +0800

    remove useless files

--- pyCADD/__init__.py
+++
@@ -1,7 +0,0 @@
-from pyCADD.utils.log import _init_log
-
-__version__= '1.5.2'
-__update_date__ = '2022-05-19'
-
-
-_init_log('pyCADD')
--- pyCADD/query/__main__.py
+++
@@ -1,4 +0,0 @@
-from pyCADD.query.cli import main
-
-if __name__ == '__main__':
-    main()
