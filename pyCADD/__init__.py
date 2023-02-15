0b79ab9801a16a4aabaaa87b44d4a6d2001c1705
CyberCatQ <yuhangxmu@foxmail.com>
2023-02-15 18:03:02 +0800

    change logger logic: Do not create log directory if there is nothing to record

--- pyCADD/__init__.py
+++ pyCADD/__init__.py
@@ -1,7 +1,7 @@
+__version__= '1.6.4dev0'
+__update_date__ = '2023-02-15'
+__author__ = 'YH. W'
+__institution__ = 'Xiamen University, China'
+
 from pyCADD.utils.log import _init_log
-
-__version__= '1.6.3'
-__update_date__ = '2022-10-27'
-
-
 _init_log('pyCADD')
