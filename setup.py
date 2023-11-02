56263ff0316845b0f647f9037d7b81301b1178d5
CyberCatQ <yuhangxmu@foxmail.com>
2023-11-02 14:57:59 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from setuptools import setup, find_packages
-with open('README.md') as f:
+with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()
 
 __version__ = "Undefined"
