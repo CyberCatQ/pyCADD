f7b1e0c5552c8f65367f0be794aaffe8de91eaba
CyberCatQ <544158656@qq.com>
2024-03-26 13:55:08 +0800

    feat: check guassian installation before use

--- pyCADD/Density/__init__.py
+++ pyCADD/Density/__init__.py
@@ -1,3 +1,4 @@
 import os
-if not os.path.exists(os.popen('which g16').read().strip()):
+from pyCADD.utils.tool import is_gaussian_available
+if not is_gaussian_available():
     print('Gaussian 16 is not installed or not in PATH.\nDensity module may not work properly.')
