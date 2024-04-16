c271e9a48a010f00e1e086f37d96499777ac67a2
CyberCatQ <544158656@qq.com>
2024-04-16 14:23:56 +0800

    refact: reorganize config for data extracting

--- pyCADD/Dock/schrodinger/config.py
+++ pyCADD/Dock/schrodinger/config.py
@@ -1,5 +1,3 @@
-from typing import Iterable
-
 FORCE_FILED_DICT = {
     "OPLS3e": "3",
     "OPLS4": "S-OPLS",
@@ -8,73 +6,52 @@ FORCE_FILED_DICT = {
     "OPLS3": "2.1"
 }
 
-class BaseConfig:
-    '''
-    配置项基类
-    '''
+
+class DefaultDataConfig:
     def __init__(self) -> None:
-        pass
+        self.properties: dict = {
+            'Docking_Score': 'r_i_docking_score',
+            'rmsd': 'r_i_glide_rmsd_to_input',
+            'ligand_efficiency': 'r_i_glide_ligand_efficiency',
+            'rotatable_bonds': 'i_i_glide_rotatable_bonds',
+            'ecoul': 'r_i_glide_ecoul',
+            'evdw': 'r_i_glide_evdw',
+            'emodel': 'r_i_glide_emodel',
+            'energy': 'r_i_glide_energy',
+            'einternal': 'r_i_glide_einternal'
+        }
 
-class DefaultDataConfig(BaseConfig):
-    '''
-    提取数据配置项
-    '''
-    def __init__(self, precision:str='SP') -> None:
+
+class SPConfig(DefaultDataConfig):
+    def __init__(self) -> None:
         super().__init__()
-        self.default_prop_XP = [
-            'PDB', 
-            'Ligand', 
-            'Original', 
-            'Docking_Score', 
-            'MMGBSA_dG_Bind', 
-            'rmsd', 
-            'precision', 
-            'Site_Score', 
-            'Volume',
-            'ligand_efficiency', 
-            'XP_Hbond', 
-            'rotatable_bonds', 
-            'ecoul', 
-            'evdw', 
-            'emodel', 
-            'energy', 
-            'einternal', 
-            'activity']
-        self.default_prop_SP = [
-            'PDB', 
-            'Ligand', 
-            'Original', 
-            'Docking_Score', 
-            'MMGBSA_dG_Bind', 
-            'rmsd', 
-            'precision', 
-            'Site_Score', 
-            'Volume', 
-            'ligand_efficiency', 
-            'rotatable_bonds', 
-            'ecoul', 
-            'evdw', 
-            'emodel', 
-            'energy', 
-            'einternal',
-            'lipo', 
-            'hbond', 
-            'metal', 
-            'rewards', 
-            'erotb', 
-            'esite', 
-            'activity'
-        ]
-        if precision == 'XP':
-            self.properties = self.default_prop_XP.copy()
-        elif precision == 'SP':
-            self.properties = self.default_prop_SP.copy()
-        else:
-            raise ValueError('Precision must be XP or SP')
+        self.properties.update({
+            'lipo': 'r_i_glide_lipo',
+            'hbond': 'r_i_glide_hbond',
+            'metal': 'r_i_glide_metal',
+            'rewards': 'r_i_glide_rewards',
+            'erotb': 'r_i_glide_erotb',
+            'esite': 'r_i_glide_esite'
+        })
 
-class DataConfig(DefaultDataConfig):
-    def __init__(self, precision, properties:list=None) -> None:
-        super().__init__(precision)
-        if isinstance(properties, Iterable):
-            self.properties.extend(properties)
+
+class XPConfig(DefaultDataConfig):
+    def __init__(self) -> None:
+        super().__init__()
+        self.properties.update({
+            'XP_Hbond': 'r_glide_XP_HBond'
+        })
+
+
+class DataConfig:
+    def __init__(self, precision: str = None, properties: dict = None) -> None:
+        if precision is None:
+            config_class = DefaultDataConfig
+        elif precision.upper() == 'SP':
+            config_class = SPConfig
+        elif precision.upper() == 'XP':
+            config_class = XPConfig
         
+        self.properties = config_class().properties
+        if isinstance(properties, dict):
+            self.properties.update(properties)
--- test/Dock/Schrodinger/test_config.py
+++ test/Dock/Schrodinger/test_config.py
@@ -1,14 +1,7 @@
 import unittest
 
-from pyCADD.Dock.schrodinger.config import (BaseConfig, DataConfig,
-                                            DefaultDataConfig)
-
-
-class TestBaseConfig(unittest.TestCase):
-    def test_init(self):
-        # Test initialization of BaseConfig
-        base_config = BaseConfig()
-        self.assertIsInstance(base_config, BaseConfig)
+from pyCADD.Dock.schrodinger.config import (DataConfig, DefaultDataConfig,
+                                            SPConfig, XPConfig)
 
 
 class TestDefaultDataConfig(unittest.TestCase):
@@ -16,18 +9,6 @@ class TestDefaultDataConfig(unittest.TestCase):
         # Test initialization of DefaultDataConfig with precision='SP'
         default_data_config = DefaultDataConfig()
         self.assertIsInstance(default_data_config, DefaultDataConfig)
-        self.assertEqual(default_data_config.properties,
-                         default_data_config.default_prop_SP)
-
-        # Test initialization of DefaultDataConfig with precision='XP'
-        default_data_config = DefaultDataConfig(precision='XP')
-        self.assertIsInstance(default_data_config, DefaultDataConfig)
-        self.assertEqual(default_data_config.properties,
-                         default_data_config.default_prop_XP)
-
-        # Test initialization of DefaultDataConfig with invalid precision
-        with self.assertRaises(ValueError):
-            DefaultDataConfig(precision='invalid')
 
 
 class TestDataConfig(unittest.TestCase):
@@ -35,26 +16,64 @@ class TestDataConfig(unittest.TestCase):
         # Test initialization of DataConfig with precision='SP' and properties=None
         data_config = DataConfig(precision='SP')
         self.assertIsInstance(data_config, DataConfig)
-        self.assertEqual(data_config.properties, data_config.default_prop_SP)
+        self.assertEqual(data_config.properties, SPConfig().properties)
 
         # Test initialization of DataConfig with precision='XP' and properties=None
         data_config = DataConfig(precision='XP')
         self.assertIsInstance(data_config, DataConfig)
-        self.assertEqual(data_config.properties, data_config.default_prop_XP)
+        self.assertEqual(data_config.properties, XPConfig().properties)
 
-        # Test initialization of DataConfig with precision='SP' and properties=list
-        properties = ['property1', 'property2']
-        data_config = DataConfig(precision='SP', properties=properties)
+        # Test initialization of DataConfig with precision=None and properties=None
+        data_config = DataConfig()
         self.assertIsInstance(data_config, DataConfig)
         self.assertEqual(data_config.properties,
-                         data_config.default_prop_SP + properties)
+                         DefaultDataConfig().properties)
 
-        # Test initialization of DataConfig with precision='XP' and properties=list
-        properties = ['property1', 'property2']
-        data_config = DataConfig(precision='XP', properties=properties)
+        # Test initialization of DataConfig with precision=None and additional properties
+        data_config = DataConfig(properties={'new_property': 'new_value'})
         self.assertIsInstance(data_config, DataConfig)
-        self.assertEqual(data_config.properties,
-                         data_config.default_prop_XP + properties)
+        test_dict = DefaultDataConfig().properties.copy()
+        test_dict.update({'new_property': 'new_value'})
+        self.assertEqual(data_config.properties, test_dict)
+
+
+class TestSPConfig(unittest.TestCase):
+    def test_properties(self):
+        sp_config = SPConfig()
+        self.assertEqual(sp_config.properties, {
+            'Docking_Score': 'r_i_docking_score',
+            'rmsd': 'r_i_glide_rmsd_to_input',
+            'ligand_efficiency': 'r_i_glide_ligand_efficiency',
+            'rotatable_bonds': 'i_i_glide_rotatable_bonds',
+            'ecoul': 'r_i_glide_ecoul',
+            'evdw': 'r_i_glide_evdw',
+            'emodel': 'r_i_glide_emodel',
+            'energy': 'r_i_glide_energy',
+            'einternal': 'r_i_glide_einternal',
+            'lipo': 'r_i_glide_lipo',
+            'hbond': 'r_i_glide_hbond',
+            'metal': 'r_i_glide_metal',
+            'rewards': 'r_i_glide_rewards',
+            'erotb': 'r_i_glide_erotb',
+            'esite': 'r_i_glide_esite'
+        })
+
+
+class TestXPConfig(unittest.TestCase):
+    def test_properties(self):
+        xp_config = XPConfig()
+        self.assertEqual(xp_config.properties, {
+            'Docking_Score': 'r_i_docking_score',
+            'rmsd': 'r_i_glide_rmsd_to_input',
+            'ligand_efficiency': 'r_i_glide_ligand_efficiency',
+            'rotatable_bonds': 'i_i_glide_rotatable_bonds',
+            'ecoul': 'r_i_glide_ecoul',
+            'evdw': 'r_i_glide_evdw',
+            'emodel': 'r_i_glide_emodel',
+            'energy': 'r_i_glide_energy',
+            'einternal': 'r_i_glide_einternal',
+            'XP_Hbond': 'r_glide_XP_HBond'
+        })
 
 
 if __name__ == '__main__':
