35c7042032a1574fb03e97e1cc523e97744a9786
Yuhang Wu <yuhang.wu@xtalpi.com>
2025-10-28 14:47:23 +0800

    Update logger

--- pyCADD/utils/log.py
+++ pyCADD/utils/log.py
@@ -11,26 +11,30 @@ class ModifiedFileHandler(ConcurrentRotatingFileHandler):
         if not os.path.exists(os.path.dirname(self.baseFilename)):
             os.makedirs(os.path.dirname(self.baseFilename))
         return super().emit(record)
-    
+
+
 def get_logfile_name():
-    """Get log file name. Generate log file name based on current date.
+    """
+    Get log file name. Generate log file name based on current date.
 
     Returns:
         str: log file name
     """
-    log_dir = os.path.join(os.getcwd(), 'logs')
-    
+    log_dir = os.path.join(os.getcwd(), "logs")
+
     date = datetime.now()
     year = str(date.year)
     month = str(date.month)
     day = str(date.day)
-    now = year + month.rjust(2, '0') + day.rjust(2, '0')
-    
+    now = year + month.rjust(2, "0") + day.rjust(2, "0")
+
     logfile_name = os.path.join(log_dir, f"{now}.log")
     return logfile_name
 
+
 def _init_log(logname):
-    """Initialize log file.
+    """
+    Initialize log file.
 
     Args:
         logname (str): logger name
@@ -41,15 +45,18 @@ def _init_log(logname):
     logger = logging.getLogger(logname)
     logger.setLevel(logging.DEBUG)
     file_fmt = logging.Formatter(
-        '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    console_fmt = logging.Formatter('%(message)s')
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s", datefmt="%Y-%m-%d %H:%M:%S"
+    )
+    console_fmt = logging.Formatter("%(message)s")
     logfile = get_logfile_name()
 
-    file_handler = ModifiedFileHandler(logfile, 'a')
+    file_handler = ModifiedFileHandler(logfile, "a")
     file_handler.setFormatter(file_fmt)
     file_handler.setLevel(logging.DEBUG)
 
-    console_handler = RichHandler()
+    console_handler = RichHandler(
+        show_path=False, rich_tracebacks=True, log_time_format="[%Y-%m-%d %H:%M:%S]"
+    )
     console_handler.setFormatter(console_fmt)
     console_handler.setLevel(logging.INFO)
 
